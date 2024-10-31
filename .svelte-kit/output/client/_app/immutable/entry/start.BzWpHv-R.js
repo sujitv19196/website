d33fd1f7453c4ce3faf3054892619ca5f1c5f014
sujit-instalily <sujit@instalily.ai>
2024-10-31 18:55:18 -0500

    remove sveltekit and add to gitignore

--- .svelte-kit/output/client/_app/immutable/entry/start.BzWpHv-R.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CPiFaWcv.js";export{t as start};
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.DJ7EBTfY.js","_app/immutable/chunks/scheduler.BfOEM8mP.js","_app/immutable/chunks/index.BOatwHNk.js","_app/immutable/chunks/index.BcIArNdp.js","_app/immutable/chunks/index.DCVOIPq2.js"];
-export const stylesheets = [];
-export const fonts = [];
